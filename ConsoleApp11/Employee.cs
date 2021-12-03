7da7a24140bb2789d070454f952c05d81dcdf7ef
Tural Taghiyev <turalrt@code.edu.az>
2021-12-03 11:45:28 +0400

    first commit

---
+++ ConsoleApp11/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    internal class Employee
+    {
+        public string Name;
+        public string Surname;
+        public string Position;
+        public int DailySalary;
+        public void MonthlySalaryCalculator(int DailySalary)
+
+        {
+            int MonthlySalary = DailySalary * 30;
+            Console.WriteLine($"MonthlySalary - {MonthlySalary}");
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instance
+            Employee employee = new Employee();
+            employee.Name = "Tural";
+            employee.Surname = "Taghiyev";
+            employee.Position = "Offshore Data Processor";
+            employee.DailySalary = 500;
+            //Console.WriteLine($"Name - {employee.Name}");
+            //Console.WriteLine($"Surname - {employee.Surname}");
+            //Console.WriteLine($"Position - {employee.Position}");
+            //Console.WriteLine($"DailySalary - {employee.DailySalary}");
+            employee.MonthlySalaryCalculator(employee.DailySalary);
+        }
+    }
+}
